65d44e8adf179e462efc8abde727a0cb7214874a
Riju Pramanik <pramanikriju@gmail.com>
2021-11-30 20:57:04 +0530

    WIP spotify integration

--- components/profile.tsx
+++ components/profile.tsx
@@ -2,13 +2,14 @@ import { Fragment } from "react";
 import { Tab } from "@headlessui/react";
 import profilePic from "../public/assets/img/me.png";
 import Image from "next/image";
+import Self from "./tabs/self";
 
-const tabsList = ["Work", "About", "Links"];
+const tabsList = ["About", "Work", "Links"];
 
 function Profile() {
   return (
     <>
-      <div className="w-64 h-64 rounded-full mx-auto max-w-64 border-2 border-blue-600 p-1">
+      <div className="w-64 h-64 rounded-full mx-auto max-w-64 border-2 border-blue-600 p-1 my-5">
         <Image
           className="rounded-full"
           src={profilePic}
@@ -16,17 +17,17 @@ function Profile() {
           layout="responsive"
         />
       </div>
-      <div className="max-w-3xl mx-auto w-full  py-8 sm:px-0 ">
+      <div className="max-w-3xl mx-auto w-full  py-8 sm:px-0 bg-blue-600/40 rounded-xl ">
         <Tab.Group>
-          <Tab.List className="flex px-5 p-1 space-x-1 bg-blue-600/40 rounded-xl border-1">
+          <Tab.List className="flex px-5 p-1 space-x-1  rounded-xl border-1">
             {tabsList.map((item, i) => (
               <Tab as={Fragment} key={i}>
                 {({ selected }) => (
                   <button
                     className={
-                      "main-font" + selected
-                        ? "transition-all  w-full py-2.5 text-sm leading-5 font-medium text-blue-700 rounded-lg focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60 bg-white shadow"
-                        : "transition-all w-full py-2.5 text-sm leading-5 font-medium text-blue-700 rounded-lg focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60  hover:bg-white/[0.12] hover:text-white"
+                      selected
+                        ? "transition-all   w-full py-2.5 text-sm leading-5 font-medium text-blue-700 rounded-lg focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60 bg-white shadow"
+                        : "transition-all w-full py-2.5 text-sm leading-5 font-medium text-white rounded-lg focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60  hover:bg-white/[0.12] hover:text-white"
                     }
                   >
                     {item}
@@ -36,7 +37,9 @@ function Profile() {
             ))}
           </Tab.List>
           <Tab.Panels className="py-2 ">
-            <Tab.Panel>Content 1</Tab.Panel>
+            <Tab.Panel>
+              <Self />
+            </Tab.Panel>
             <Tab.Panel>Content 2</Tab.Panel>
             <Tab.Panel>Content 3</Tab.Panel>
           </Tab.Panels>
---
+++ components/tabs/WorkHistory.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function WorkHistory(props) {
+  return <div>History Goes here</div>;
+}
+
+export default WorkHistory;
